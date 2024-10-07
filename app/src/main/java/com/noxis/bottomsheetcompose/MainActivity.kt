cff8b28f57a260312611958c3f202e40b4dbb4df
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2024-10-07 19:24:00 +0300

    Creating the Top Bar

--- app/src/main/java/com/noxis/bottomsheetcompose/MainActivity.kt
+++ app/src/main/java/com/noxis/bottomsheetcompose/MainActivity.kt
@@ -5,12 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.noxis.bottomsheetcompose.ui.theme.BottomSheetComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +26,28 @@ class MainActivity : ComponentActivity() {
         setContent {
             BottomSheetComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    //TODO
                 }
             }
         }
     }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar() {
+    TopAppBar(
+        title = { Text(text = stringResource(R.string.app_name), fontSize = 18.sp) },
+        colors = TopAppBarDefaults.topAppBarColors(
+            titleContentColor = Color.White,
+            containerColor = colorResource(id = R.color.purple_200)
+        )
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TopBarPreview() {
+    TopBar()
 }
